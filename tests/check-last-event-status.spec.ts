738b65b52539017a08095c2a9279cb1e6d831f3b
Rodrigo <rodrigorael53@gmail.com>
2022-05-08 03:03:20 -0300

    implement the test

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -2,17 +2,29 @@
 class CheckLastEventStatus {
   constructor (private readonly loadLastEventRepository: LoadLastEventRepository) {}
   async perform (groupId: string): Promise<void> {
-    this.loadLastEventRepository.groupId = groupId;
+    await this.loadLastEventRepository.loadLastEvent(groupId);
   }
 }
 
-class LoadLastEventRepository {
+// interface é para definir um contrato
+interface LoadLastEventRepository {
+  loadLastEvent: (groupId: string) => Promise<void>;
+}
+
+// Mock é um duble de teste que está precupado com o in put, precupado em só ter
+// variáveis auciliares para poder fazer comparações, só com o in put 
+class LoadLastEventRepositoryMock implements LoadLastEventRepository {
   groupId?: string;
+
+  async loadLastEvent (groupId: string): Promise<void> {
+    // quando eu faço essa atribuição eu consigo fazer uma comparação no teste
+    this.groupId = groupId
+  }
 }
 
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepository();
+    const loadLastEventRepository = new LoadLastEventRepositoryMock();
     const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository);
 
     await checkLastEventStatus.perform('any_group_id');
