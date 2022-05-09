a55bb4194855005cbbdd335435e855606eaddda2
Rodrigo <rodrigorael53@gmail.com>
2022-05-09 00:35:47 -0300

    ensure CheckLastEventStatus returns status done when group has no event

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 - Improper Instantiation: criar instâncias de forma errada 
 - High Coupling: quando uma classe criar sua propria dependência
 - Test Code in Production
+- Duplicate Code
+- Shotgun Surgery
 
 <!-- Coias boas que deveriamos seguir para colocar no código, coisas que já foram validados por pessaos no passado que já garantiram que é uma forma boa de programar -->
 # Design Patterns/Principles/Conventions
@@ -16,6 +18,7 @@
 - Arrange, Act, Assert (AAA): todo teste sempre é criado um bloco onde é ordanizado o teste, depois temos uma ação para se tomar e baseado naquela ação vamos testar alguma coisa
 - Dependency Injection (DI): ao invés da minha classe criar as suas proprias dependências, nós recebemos essas dependências de alguém
 - Repository Pattern
-- Test Doubles (Mock)
+- Test Doubles (Mock, Stub, Spy)
 - Small Commits
 - System Under Test (SUT)
+- Strategy Pattern
--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,32 +1,36 @@
 // useCase
 class CheckLastEventStatus {
   constructor (private readonly loadLastEventRepository: LoadLastEventRepository) {}
-  async perform (groupId: string): Promise<void> {
+  async perform (groupId: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupId);
+    return 'done';
   }
 }
 
 // interface é para definir um contrato
 interface LoadLastEventRepository {
-  loadLastEvent: (groupId: string) => Promise<void>;
+  loadLastEvent: (groupId: string) => Promise<undefined>;
 }
 
-// Mock é um duble de teste que está precupado com o in put, precupado em só ter
-// variáveis auciliares para poder fazer comparações, só com o in put 
-class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+// Mock é um duble de teste que está precupado com o input, precupado em só ter
+// variáveis auciliares para poder fazer comparações, só com o input
+// o Spy eu me precupo tanto com o input quanto o output
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
   groupId?: string;
   callsCount = 0;
+  output: undefined;
 
-  async loadLastEvent (groupId: string): Promise<void> {
+  async loadLastEvent (groupId: string): Promise<undefined> {
     // quando eu faço essa atribuição eu consigo fazer uma comparação no teste
     this.groupId = groupId
     this.callsCount++
+    return this.output;
   }
 }
 
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock();
+    const loadLastEventRepository = new LoadLastEventRepositorySpy();
     // sut = System Under Test
     const sut = new CheckLastEventStatus(loadLastEventRepository);
 
@@ -34,5 +38,16 @@ describe('CheckLastEventStatus', () => {
 
     expect(loadLastEventRepository.groupId).toBe('any_group_id');
     expect(loadLastEventRepository.callsCount).toBe(1);
+  });
+
+  it('should return status done when group has no event', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy();
+    loadLastEventRepository.output = undefined;
+    // sut = System Under Test
+    const sut = new CheckLastEventStatus(loadLastEventRepository);
+
+    const status = await sut.perform('any_group_id');
+
+    expect(status).toBe('done');
   });
 });
